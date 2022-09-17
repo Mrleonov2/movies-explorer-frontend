13f3aae4e1645b6b1befad851fa90f97c6f1928a
Mrleonov2 <mikhail-leonov2002@yandex.ru>
2022-09-17 17:36:52 +0700

    init struct

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">
+    <Route path="/">
+    </Route>
+    <Route path="/movies">
+    </Route>
+    <Route path="/saved-movies">
+    </Route>
+    <Route path="/profile">
+    </Route>
+    <Route path="/signin">
+    </Route>
+    <Route path="/signup">
+    </Route>
+  </div>;
+}
+
+export default App;
